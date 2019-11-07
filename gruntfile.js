f85b30f966216091f3a75cc87c2218f9cae17583
Kevin Booth <kbooth@gykantler.com>
2019-11-07 15:34:02 -0500

    Setup grunt file and package.json

---
+++ gruntfile.js
@@ -0,0 +1,101 @@
+'use strict';
+
+var pkgjson = require('./package.json');
+
+var config = {
+    pkg: pkgjson,
+};
+module.exports = function (grunt) {
+    grunt.initConfig({
+        cacheBust: {
+            options: {
+                queryString: true
+            },
+            full: {
+                options: {
+                    assets: ['static/styles/**', 'static/scripts/**', 'vendor/**'],
+                },
+                src: ['index.html']
+            },
+            css: {
+                options: {
+                    assets: ['static/styles/**'],
+                },
+                src: ['index.html']
+            },
+            js: {
+                options: {
+                    assets: ['static/scripts/**'],
+                },
+                src: ['index.html']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'static/images/',
+                    src: ['**/*.{gif}'], //{png,jpg,gif}
+                    dest: 'static/images/'
+                }]
+            }
+        },
+        jshint: {
+            options: {
+                strict: false
+            },
+            files: ['static/scripts/js/*.js']
+        },
+        sass: {
+            options: {
+                style: 'compressed',
+            },
+            compile: {
+                files: {
+                    'static/styles/styles.min.css': 'static/styles/scss/styles.scss',
+                }
+            }
+        },
+        uglify: {
+            options: {
+                srcMap: true
+            },
+            compile: {
+                files: {
+                    'static/scripts/common.min.js': ['static/scripts/js/scripts.js']
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: ['static/styles/*.scss', 'static/styles/*.scss'],
+                tasks: ['sass']//'imagemin', 'cacheBust:css']
+            },
+            scripts: {
+                files: ['<%= jshint.files %>'],
+                tasks: ['jshint', 'uglify','imagemin', 'cacheBust:js']
+            }
+        }
+    });
+
+
+    grunt.registerTask('init', [
+        'move'
+    ]);
+    grunt.registerTask('optimize', [
+		'sass',
+		'jshint',
+		'uglify',
+		'imagemin',
+        'cacheBust:full'
+    ]);
+    grunt.registerTask('default', []);
+
+    // Load Tasks
+    grunt.loadNpmTasks('grunt-cache-bust');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
