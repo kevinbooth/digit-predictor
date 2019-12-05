308fffd117dd973275ff5feea97597b8be42878f
Kevin Booth <kdbooth97@gmail.com>
2019-12-05 18:38:43 -0500

    Build frontend user interface and drawing functionality

--- static/scripts/js/app.js
+++ static/scripts/js/app.js
@@ -11,7 +11,12 @@
  * DR Digit Reader
  * @namespace
  */
-var DR = window.DR || {};
+var DR = window.DR || {
+    canvas: '',
+    context: '',
+    mouse: '',
+    prevMouse: ''
+};
 
 /**
  * Checks if DOM is ready for JS to fire
@@ -31,7 +36,67 @@ DR.ready = function (_fn) {
  * @function
  */
 DR.init = function () {
+    DR.initDrawing();
+};
 
+/**
+ * Initiates the drawing functionality within the canvas
+ * @function
+ */
+DR.initDrawing = function () {
+    /* Setup variables and configurations */
+    DR.canvas = document.querySelector("#canvas");
+    DR.context = canvas.getContext("2d");
+    DR.canvas.width = 280;
+    DR.canvas.height = 280;
+    DR.mouse = {
+        x: 0, 
+        y: 0
+    };
+    DR.prevMouse = {
+        x: 0, 
+        y: 0
+    };
+    DR.context.fillStyle = "#232323";
+	DR.context.fillRect(0, 0, DR.canvas.width, DR.canvas.height);
+	DR.context.color = "white";
+	DR.context.lineWidth = 20;
+    DR.context.lineJoin = DR.context.lineCap = 'round';
+
+	DR.canvas.addEventListener("mousemove", function(e) {
+		DR.prevMouse.x = DR.mouse.x;
+		DR.prevMouse.y = DR.mouse.y;
+
+		DR.mouse.x = e.pageX - this.offsetLeft;
+		DR.mouse.y = e.pageY - this.offsetTop;
+	}, false);
+
+	DR.canvas.addEventListener("mousedown", function(e) {
+		DR.canvas.addEventListener("mousemove", DR.draw, false);
+	}, false);
+
+	DR.canvas.addEventListener("mouseup", function() {
+		DR.canvas.removeEventListener("mousemove", DR.draw, false);
+	}, false);    
+
+
+};
+
+/**
+ * Fires off all functions on page load
+ * @function
+ */
+DR.draw = function () {
+    DR.context.lineWidth = DR.context.lineWidth;
+    DR.context.lineJoin = "round";
+    DR.context.lineCap = "round";
+    DR.context.strokeStyle = DR.context.color;
+
+    DR.context.beginPath();
+    DR.context.moveTo(DR.prevMouse.x, DR.prevMouse.y);
+    DR.context.lineTo(DR.mouse.x, DR.mouse.y );
+    DR.context.closePath();
+    DR.context.stroke();
 };
 
 /**
